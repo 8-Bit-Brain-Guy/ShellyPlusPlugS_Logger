12b19b9de0c754549b2185e1cc47f5767ee51520
8-Bit-Brain-Guy <>
2025-06-15 10:48:42 +0200

    1

--- src/ShellyPlusPlusS_Logger.py
+++ src/ShellyPlusPlusS_Logger.py
@@ -1,1 +1,18 @@
 # ShellyPlusPlusS_Logger.py
+
+import time
+import logging
+import re
+import json
+from typing import NamedTuple
+import paho.mqtt.client as mqtt
+import os
+
+MQTT_SERVER = os.environ['MQTT_Server']
+MQTT_PORT = 8883
+MQTT_USER = os.environ['MQTT_USER']
+MQTT_PASSWORD = os.environ['MQTT_PASSWORD']
+MQTT_TOPIC = 'shellyplusplugs/events/rpc'
+
+
+
