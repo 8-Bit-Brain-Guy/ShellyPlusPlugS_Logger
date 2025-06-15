6322fc3c2a3ffa93b58289b85d89df9c2aeb3ff7
8-Bit-Brain-Guy <>
2025-06-15 10:55:09 +0200

    1

--- src/ShellyPlusPlusS_Logger.py
+++ src/ShellyPlusPlusS_Logger.py
@@ -8,11 +8,19 @@ from typing import NamedTuple
 import paho.mqtt.client as mqtt
 import os
 
+
+# Set this variables in Linux using: export MQTT_Server=12678467823
+# Show them using:                   echo $MQTT_Server
+
 MQTT_SERVER = os.environ['MQTT_Server']
 MQTT_PORT = 8883
 MQTT_USER = os.environ['MQTT_USER']
 MQTT_PASSWORD = os.environ['MQTT_PASSWORD']
 MQTT_TOPIC = 'shellyplusplugs/events/rpc'
 
+
+print(MQTT_SERVER)
+print(MQTT_USER)
+print(MQTT_PASSWORD)
 
 
