d196b40f7670761f45e9210f4de7be09fd7b9305
8-Bit-Brain-Guy <>
2025-06-15 10:58:09 +0200

    1

--- src/ShellyPlusPlusS_Logger.py
+++ src/ShellyPlusPlusS_Logger.py
@@ -12,14 +12,14 @@ import os
 # Set this variables in Linux using: export MQTT_Server=12678467823
 # Show them using:                   echo $MQTT_Server
 
-MQTT_SERVER = os.environ['MQTT_Server']
+MQTT_SERVER = os.environ['MQTT_SERVER']
 MQTT_PORT = 8883
 MQTT_USER = os.environ['MQTT_USER']
 MQTT_PASSWORD = os.environ['MQTT_PASSWORD']
 MQTT_TOPIC = 'shellyplusplugs/events/rpc'
 
-print("MQTT_SERVER: " %MQTT_SERVER)
-print("MQTT_USER:   " %MQTT_USER)
+print("MQTT_SERVER: %s" %MQTT_SERVER)
+print("MQTT_USER:   %s" %MQTT_USER)
 
 
 
